c73d8989df83fa2073e7989e513e45a312354306
Eldors <56890446+Eldors@users.noreply.github.com>
2021-08-14 19:55:52 +0300

    [OYC-15] feat: styles contained & outlined button

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core
 })
 
 export class ButtonComponent implements OnInit {
-  @Input() severity: 'primary' | 'secondary' = 'primary';
+  @Input() severity: 'primary' | 'secondary' | 'warning' | 'danger' | 'success' = 'primary';
+  @Input() shape: 'contained' | 'outlined' = 'contained';
 
   constructor() { }
 
