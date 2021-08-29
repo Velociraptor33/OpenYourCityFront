ec31259cc1107340f440f838ef90bd385affee32
Eldors <56890446+Eldors@users.noreply.github.com>
2021-08-29 15:53:05 +0300

    [OYC-17] feat: Implemented processing of button click

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'oyc-button',
@@ -13,5 +13,11 @@ export class ButtonComponent {
   @Input() disabled: true | false = false;
   @Input() rippleColor: string = 'rgba(36, 34, 34, 0.7)';
 
+  @Output() clicked: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+
   constructor() { }
+
+  clickEvent(event: MouseEvent): void {
+    this.clicked.emit(event);
+  }
 }
